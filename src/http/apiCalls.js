f8d17d42efbd4facfc5ff2ad193148d00cf157da
yauhenibh <yauhenibh@gmail.com>
2018-09-02 20:58:52 +0300

    updated project ideas created pseudo code for api calls

---
+++ src/http/apiCalls.js
@@ -0,0 +1,16 @@
+/*export const APICalls ={
+    getAllSubjects: {
+        method:"GET",
+        path:"/api/1231"
+    }
+}
+
+export const httpService = (params, props) =>{
+ //run call
+ return Axios[params.method](params.path);
+}
+
+
+httpsService(APICalls.getAllSubjects, {
+    
+}).then(response, error)*/
